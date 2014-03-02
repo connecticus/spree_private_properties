9252e28196252e9e9da43cc2585093e53f31adca
Willian van der Velde <willian@pythonheads.nl>
2014-03-02 16:49:09 +0100

    After filter wont work :)

--- app/controllers/spree/product_controller_decorator.rb
+++ app/controllers/spree/product_controller_decorator.rb
@@ -1,8 +1,12 @@
 Spree::ProductsController.class_eval do
-  after_filter :only_show_visible_product_properties, only: [:show]
-
-  private
-  def only_show_visible_product_properties
-    @product_properties = @product_properties.joins(:property).where("spree_properties.visible = ?", true)
+  def show_without_private_properties
+    return_value = show_with_private_properties
+    @product_properties = @product_properties
+                            .joins(:property)
+                            .where("spree_properties.visible = ?", true)
+    return_value
   end
+
+  alias_method :show_with_private_properties, :show
+  alias_method :show, :show_without_private_properties
 end
